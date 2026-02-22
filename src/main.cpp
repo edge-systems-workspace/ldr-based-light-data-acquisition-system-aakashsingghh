c1ca92807c8bc604691f174ac010031ba63941b7
aakashsingghh <akashsingh33280@gmail.com>
2026-02-22 23:47:54 +0530

    Logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 int ldrPin=A0;
-// void setup() {
-//     serial.begin(9600);
-//     pinMode(ldrPin,INPUT);
-//
-// }
-//
-// void loop() {
-//     int ldrValue=analogRead(ldrPin);
-//     int lightPercent=map(ldrValue,0,1023,0,100);
-//     serial.println(ldrValue);
-//     delay(1000);
-// }
+void setup() {
+    serial.begin(9600);
+    pinMode(ldrPin,INPUT);
+
+}
+
+void loop() {
+    int ldrValue=analogRead(ldrPin);
+    int lightPercent=map(ldrValue,0,1023,0,100);
+    serial.println(ldrValue);
+    delay(1000);
+}
 
